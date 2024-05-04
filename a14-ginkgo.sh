ce09f9cdfbdd946cdc5bbc4a37d14b39d665a983
ozturkmutlu65 <155987147+ozturkmutlu65@users.noreply.github.com>
2024-05-04 03:10:03 +0300

    Update a14-ginkgo.sh

--- a14-ginkgo.sh
+++ a14-ginkgo.sh
@@ -2,7 +2,7 @@ rm -rf .repo/local_manifests/
 #repo init rom
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 #Local manifests
-git clone https://github.com/ozturkmutlu65/local_manifest-b main .repo/local_manifests
+git clone https://github.com/ozturkmutlu65/local_manifest -b main .repo/local_manifests
 
 #build
 /opt/crave/resync.sh
